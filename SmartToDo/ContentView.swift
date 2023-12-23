717422f3fc4a41568b4da01519e783f9a4e2b973
keremoflu <keremoflu92@gmail.com>
2023-12-23 20:13:38 +0300

    Initial Commit

---
+++ SmartToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartToDo
+//
+//  Created by user on 23.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartToDo/SmartToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartToDoApp.swift
+//  SmartToDo
+//
+//  Created by user on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
